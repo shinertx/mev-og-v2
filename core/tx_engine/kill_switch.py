2db16a91f24e76b7d7b255f18f8a33bc25d4d714
shinertx <168613225+shinertx@users.noreply.github.com>
2025-05-26 06:43:52 -0500

    Delete core/tx_engine/kill_switch.py

--- core/tx_engine/kill_switch.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def kill_switch_triggered() -> bool:
-    """Return True if kill switch file or env variable is present."""
-    if os.environ.get('KILL_SWITCH_ACTIVE', '0') == '1':
-        return True
-    return os.path.exists('kill_switch.flag')
